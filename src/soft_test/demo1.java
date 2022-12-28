0932dd9bf5218d2b9e92b5025f1a737275e5f7dd
SSK <SSK@SSK-PC>
2022-12-28 17:06:15 +0530

    [Avinash Todakri]: 1st change in project.

---
+++ src/soft_test/demo1.java
@@ -0,0 +1,9 @@
+package soft_test;
+
+public class demo1
+{
+public static void main(String[] args) 
+{
+	System.out.println("github practice");
+}
+}
---
+++ src/soft_test/demo2.java
@@ -0,0 +1,9 @@
+package soft_test;
+
+public class demo2
+{
+public static void main(String[] args) 
+{
+	System.out.println("git test");
+}
+}
